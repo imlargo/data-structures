f8664381d3eee9ab9780c4745fb8b79e0fd6bd83
Juan Carlos Largo <134282467+imlargo@users.noreply.github.com>
2024-03-13 10:44:59 -0500

    _

--- week-5/lab-4.py
+++ week-5/lab-4.py
@@ -1,3 +1,223 @@
+from typing import List
+
+class Usuario:
+    def __init__(self, nombre: str = None, idUsuario: str = None):
+        self.nombre = nombre
+        self.id = idUsuario
+        
+        self.fecha_nacimiento: Fecha = None
+        self.ciudad_nacimiento: str = None
+        self.tel: str = None
+        self.email: str = None
+        self.dir: Direccion = None
+    
+    # Setter methods
+    def setNombre(self, nombre):
+        self.nombre = nombre
+    def setId(self, id):
+        self.id = int(id)
+    def setFecha_nacimiento(self, fecha):
+        self.fecha_nacimiento = fecha
+    def setCiudad_nacimiento(self, ciudad):
+        self.ciudad_nacimiento = ciudad
+    def setTel(self, telefono):
+        self.tel = telefono
+    def setEmail(self, email):
+        self.email = email
+    def setDir(self, dir):
+        self.dir = dir
+    
+    # Getter methods
+    def getNombre(self):
+        return self.nombre
+    
+    def getId(self):
+        return self.id
+    
+    def getFecha_nacimiento(self):
+        return self.fecha_nacimiento
+    
+    def getCiudad_nacimiento(self):
+        return self.ciudad_nacimiento
+    
+    def getTel(self):
+        return self.tel
+    
+    def getEmail(self):
+        return self.email
+    
+    def getDir(self):
+        return self.dir
+    
+    # String representation
+    def __str__(self):
+        return f'{self.nombre} - {self.id} - {self.fecha_nacimiento} - {self.ciudad_nacimiento} - {self.tel} - {self.email} - {self.dir}'
+    
+class Fecha:
+    def __init__(self, dd = None, mm = None, aa = None):
+        self.dd = dd
+        self.mm = mm
+        self.aa = aa
+        pass
+
+    def setDia(self, dd):
+        self.dd = dd
+    def setMes(self, mm):
+        self.mm = mm
+    def setA(self, aa):
+        self.aa = aa
+
+    def getDia(self):
+        return self.dd
+    def getMes(self):
+        return self.mm
+    def getA(self):
+        return self.aa
+    
+    def __str__(self):
+        return f'{self.dd}/{self.mm}/{self.aa}'
+    
+class Direccion:
+    def __init__(self, calle: str = None, nomenclatura: str = None, barrio: str = None, ciudad: str = None, edificio: str = None, apto: str = None):
+        self.calle: str = calle
+        self.nomenclatura: str = nomenclatura
+        self.barrio: str = barrio
+        self.ciudad: str = ciudad
+        self.edificio: str = edificio
+        self.apto: str = apto
+    
+    def setCalle(self, calle):
+        self.calle = calle
+    def setNomenclatura(self, n):
+        self.nomenclatura = n
+    def setBarrio(self, b):
+        self.barrio = b
+    def setCiudad(self, ci):
+        self.ciudad = ci
+    def setEdificio(self, e):
+        self.edificio = e
+    def setApto(self, a):
+        self.apto = a
+    def getCalle(self):
+        return self.calle
+    def getNomenclatura(self):
+        return self.nomenclatura
+    def getBarrio(self):
+        return self.barrio
+    def getCiudad(self):
+        return self.ciudad
+    def getEdificio(self):
+        return self.edificio
+    def getApto(self):
+        return self.apto
+    
+    def __str__(self):
+        return f'{self.calle}, {self.nomenclatura}, {self.barrio}, {self.ciudad}, {self.edificio}, {self.apto}'
+
+class Utils:
+    
+    predefinidos = [
+        "Juan - 123 - 16/7/2005 - Valledupar - 123456789 - imlargo@unal - Calle 6, Cra 6, Barrio Robledo, Ciudad 1, Edificio 1, Apto 100",
+        "Alejandra - 201 - 16/7/2005 - Valledupar - 987654321 - aleja@unal - Calle 5, Cra 5, Barrio 1, Ciudad 1, Edificio 2, Apto 201",
+        "Harrison - 302 - 16/7/2005 - Valledupar - 12349876 - harri@unal - Calle 4, Cra 4, Barrio 1, Ciudad 1, Edificio 3, Apto 302",
+        "JulianMoreno - 403 - 16/7/2005 - Valledupar - 98712340 - juliangod@unal - Calle 3, Cra 3, Barrio 1, Ciudad 1, Edificio 4, Apto 403",
+        "TaylorSwift - 504 - 16/7/2005 - Valledupar - 19283745 - taylor@unal - Calle 2, Cra 2, Barrio 1, Ciudad 1, Edificio 5, Apto 504",
+    ]
+
+    @classmethod
+    def convertStringToUser(cls, informacion: str) -> Usuario:
+        
+        # Estrucutra de la informacion de usuario: nombre, id, fecha_nacimiento, ciudad_nacimiento, tel, email, dir
+        datosUsuario = informacion.split(' - ')
+        
+        # Estructura de la fecha dd/mm/aa
+        fecha = cls.convertStringToDate(datosUsuario[2])
+        
+        # Estructura de la direccion: calle, nomenclatura, barrio, ciudad, edificio, apto
+        direccion = cls.convertStringToDireccion(datosUsuario[6])
+        
+        usuario = Usuario(datosUsuario[0], int(datosUsuario[1]))
+        usuario.setFecha_nacimiento(fecha)
+        usuario.setCiudad_nacimiento(datosUsuario[3])
+        usuario.setTel(datosUsuario[4])
+        usuario.setEmail(datosUsuario[5])
+        usuario.setDir(direccion)
+        return usuario
+    
+    @classmethod
+    def convertStringToDate(cls, fecha: str) -> Fecha:
+        # Separar la fecha en dia, mes y año
+        datosFecha = fecha.split('/')
+        return Fecha(int(datosFecha[0]), int(datosFecha[1]), int(datosFecha[2]))
+    
+    @classmethod
+    def convertStringToDireccion(cls, direccion: str) -> Direccion:
+        # Separar la direccion en sus componentes: calle, nomenclatura, barrio, ciudad, edificio, apto
+        datosDireccion = direccion.split(', ')
+        return Direccion(datosDireccion[0], datosDireccion[1], datosDireccion[2], datosDireccion[3], datosDireccion[4], datosDireccion[5])
+
+class Agenda:
+
+    def __init__(self, capacidad: int = None):
+        self._registro: List[Usuario] = [ None for x in range(capacidad) ]
+        self._no_reg: int = 0
+        self._capacidad: int = capacidad
+        pass
+
+    def agregar(self, usuario: Usuario) -> bool:
+        inAgenda = self.buscar(usuario.getId()) != -1
+        if (not inAgenda) and (self._no_reg < self._capacidad):
+            self._registro[self._no_reg] = usuario
+            self._no_reg += 1
+            return True
+        else:
+            return False
+        pass
+
+    def buscar(self, userId: str) -> int:
+        for i in range(self._no_reg):
+            user = self._registro[i]
+            if user == None:
+                return -1
+            if user.getId() == userId:
+                return i
+        return -1
+
+    def eliminar(self, userId: str) -> bool:
+        index = self.buscar(userId)
+        if index == -1:
+            return False
+
+        for i in range(index + 1, self._no_reg):
+            self._registro[i-1] = self._registro[i]
+
+        self._registro[self._no_reg-1] = None
+        self._no_reg -= 1
+        return True
+
+    def toFile(self) -> None:
+        texto = ""
+        for i in range(self._no_reg):
+            texto += f"{self._registro[i].__str__()}\n" if i < self._no_reg - 1 else f"{self._registro[i].__str__()}"
+        
+        file = open('agenda.txt', 'w')
+        file.write(texto)
+        file.close()
+        pass
+
+    def importar(self):
+        file = open('agenda.txt', 'r')        
+        datos = file.read().split('\n')
+        file.close()
+        
+        for informacion in datos:
+            usuario = Utils.convertStringToUser(informacion)
+            self.agregar(usuario)
+        pass
+
+
+#....
+
 class SimpleNode:
     def __init__(self, data = None):
         self.data = data
@@ -188,6 +408,18 @@ def main_1():
         siguiente = siguiente.getNext()
 
 def main_2():
-    pass
+    listaDoble = SimpleDoble()
+    listaDoble.addFirst(
+        Utils.convertStringToUser(Utils.predefinidos[0])
+    )
+    
+    for userString in Utils.predefinidos:
+        user = Utils.convertStringToUser(userString)
+        listaDoble.addLast(user)
+
+    node = listaDoble.first()
+    while node != None:
+        print(node.getData())
+        node = node.getNext()
 
 main_1()
