fdd0b491a34203b9fba2102371c047e4c8a11cfb
Juan Carlos Largo <134282467+imlargo@users.noreply.github.com>
2024-08-09 20:05:15 +0000

    implementar cola

---
+++ go/lab-7/cola.go
@@ -0,0 +1,22 @@
+package main
+
+type Cola[T any] struct {
+	lista *ListaSimple[T]
+}
+
+func (cola *Cola[T]) getSize() int {
+	return cola.lista.getSize()
+}
+func (cola *Cola[T]) isEmpty() bool {
+	return cola.lista.isEmpty()
+}
+func (cola *Cola[T]) enqueue(data *NodoSimple[T]) {
+	cola.lista.addLast(data)
+}
+func (cola *Cola[T]) dequeue() T {
+	data := cola.lista.removeFirst()
+	return data
+}
+func (cola *Cola[T]) first() T {
+	return cola.lista.first().getData()
+}
