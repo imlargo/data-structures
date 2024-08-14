e6524026b7ac4628bf2be72029fa58572e0c1e90
Juan Carlos Largo <134282467+imlargo@users.noreply.github.com>
2024-08-14 18:47:45 +0000

    implementar algoritmos de heap

--- go/lab-7-heaps/heap.go
+++ go/lab-7-heaps/heap.go
@@ -3,7 +3,7 @@ package main
 import "math"
 
 type Heap[T any] struct {
-	data []T
+	data []int
 	size int
 }
 
@@ -23,12 +23,50 @@ func (heap *Heap[T]) right(i int) int {
 	return (2 * i) + 2
 }
 
-func (heap *Heap[T]) max_heapify(array []int, index int, size int) {
-	println("Max heapify")
+func (heap *Heap[T]) max_heapify(index int) []int {
+	left := heap.left(index)
+	right := heap.right(index)
+
+	var largest int
+
+	if left < heap.size && heap.data[left] > heap.data[index] {
+		largest = left
+	} else {
+		largest = index
+	}
+
+	if right < heap.size && heap.data[right] > heap.data[largest] {
+		largest = right
+	}
+
+	if largest != index {
+		temp := heap.data[index]
+		heap.data[index] = heap.data[largest]
+		heap.data[largest] = temp
+		heap.data = heap.max_heapify(largest)
+	}
+
+	return heap.data
 }
+
 func (heap *Heap[T]) build_max_heap() {
-	println("Building max heap")
+
+	var mid int = int(math.Floor(float64(heap.size) / 2))
+
+	for i := mid; i >= 0; i-- {
+		heap.data = heap.max_heapify(i)
+	}
 }
+
 func (heap *Heap[T]) heap_sort() {
-	println("Heap sort")
+	heap.build_max_heap()
+
+	for i := heap.size - 1; i >= 1; i-- {
+		temp := heap.data[i]
+		heap.data[i] = heap.data[0]
+		heap.data[0] = temp
+
+		heap.size = heap.size - 1
+		heap.data = heap.max_heapify(0)
+	}
 }
