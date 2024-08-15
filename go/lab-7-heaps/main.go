beea1bddcb2f6e5200b1aeaf06fb2b82c9ecc521
Juan Carlos Largo <134282467+imlargo@users.noreply.github.com>
2024-08-15 13:36:29 +0000

    agregar caso de prueba de heap

--- go/lab-7-heaps/main.go
+++ go/lab-7-heaps/main.go
@@ -1,7 +1,38 @@
 package main
 
+import (
+	"math/rand/v2"
+)
+
 func main() {
-	var heap Heap[int] = Heap[int]{data: []int{1, 2, 3, 4, 5}, size: 5}
+
+	var cantidad int = 20
+
+	var data []int = make([]int, cantidad)
+	for i := 0; i < cantidad; i++ {
+		var num int = rand.IntN(100)
+		data[i] = num
+	}
+
+	// Imprimir el arreglo
+	println("Arreglo: ")
+	for i := 0; i < cantidad; i++ {
+		print(data[i], ", ")
+	}
+
+	println("\n\n")
+
+	var heap Heap[int] = Heap[int]{data: data, size: cantidad}
+
+	heap.build_max_heap()
+
+	// Imprimir el arreglo
+	println("Max heap: ")
+	for i := 0; i < heap.size; i++ {
+		print(heap.data[i], ", ")
+	}
+
+	println("\n\n")
 
 	println("Heap size: ", heap.getSize())
 }
