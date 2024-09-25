cace1e1f3badfa43b541a155f9682b405264d0a9
imlargo <jlargob@unal.edu.co>
2024-09-25 08:46:54 -0500

    finalizar lab 9

--- go/labs/lab-9/main.go
+++ go/labs/lab-9/main.go
@@ -6,19 +6,28 @@ import (
 
 func main() {
 
-	println("\n\n--- Problema #2 ---\n\n")
+	println("\n\n# - - - Problema #2 - - - #\n\n")
 
-	problema2()
+	println("\n--- Usando hash de division ---\n")
+	problema2(hash_table.HashFuncDivision)
+
+	println("\n--- Usando hash de multiplicacion ...\n")
+	problema2(hash_table.HashFuncMultiplicacion)
 
 	println("\n\n--- Problema #3 ---\n\n")
 
-	problema3()
+	println("\n--- Usando hash de division ---\n")
+	problema3(hash_table.HashFuncDivision)
+
+	println("\n--- Usando hash de multiplicacion ...\n")
+	problema3(hash_table.HashFuncMultiplicacion)
+
 }
 
-func problema2() {
+func problema2(selectedHashFunc func(int, int) int) {
 
 	var size int = 10
-	hashTable := hash_table.New[int](size, hash_table.HashFuncDivision)
+	hashTable := hash_table.New[int](size, selectedHashFunc)
 
 	// Crear arreglo de numeros aleatorios
 	var nums []int = crearArregloAleatorio(20)
@@ -38,35 +47,24 @@ func problema2() {
 	println("Resultado: ", result)
 
 	// Eliminar un numero
-	println("Eliminando numero: ", num)
+	println("Eliminando numero", num, "en index", hashTable.HashFunc(num, hashTable.Size))
 	hashTable.Delete(num)
+
+	// Mostrar distribucion
+	hashTable.MostrarDistribucion()
 
 }
 
-func problema3() {
+func problema3(selectedHashFunc func(int, int) int) {
 	var size int = 5
 	var usuarios []*Usuario = getUsuarios()
 
 	/* Crear tabla hash con HashFuncDivision */
-	hashTable1 := hash_table.New[*Usuario](size, hash_table.HashFuncDivision)
+	hashTable := hash_table.New[*Usuario](size, selectedHashFunc)
 	for _, usuario := range usuarios {
 		println("Insertando usuario: ", usuario.Id, usuario.Nombre)
-		hashTable1.Insert(usuario.Id, usuario)
+		hashTable.Insert(usuario.Id, usuario)
 	}
-	// Mostrar contadores
-	for i, lista := range hashTable1.Array {
-		println(i, " -> ", lista.Size)
-	}
-
-	/* Crear tabla hash con HashFuncMultiplicacion */
-	hashTable2 := hash_table.New[*Usuario](size, hash_table.HashFuncMultiplicacion)
-	for _, usuario := range usuarios {
-		println("Insertando usuario: ", usuario.Id, usuario.Nombre)
-		hashTable2.Insert(usuario.Id, usuario)
-	}
-	// Mostrar contadores
-	for i, lista := range hashTable2.Array {
-		println(i, " -> ", lista.Size)
-	}
-
+	// Mostrar distribucion
+	hashTable.MostrarDistribucion()
 }
--- go/structures/hash_table/hash_func.go
+++ go/structures/hash_table/hash_func.go
@@ -14,3 +14,9 @@ func HashFuncMultiplicacion(value int, m int) int {
 	v := va - math.Floor(va)               // Tomar la parte decimal
 	return int(math.Floor(float64(m) * v)) // Multiplicar por m y tomar la parte entera
 }
+
+func (hashTable *ChainedHashTable[T]) MostrarDistribucion() {
+	for i, lista := range hashTable.Array {
+		println(i, " -> ", lista.Size)
+	}
+}
